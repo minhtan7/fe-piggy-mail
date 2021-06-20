4228d13822bfce89975d6613291b12cd7d15850e
Your Name <you@example.com>
2021-06-20 19:46:35 +0700

    fetch test data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as jwt from "jsonwebtoken";
 
 import ProtectedRoute from "./routes/PrivateRoute";
 import LoginPage from "./pages/LoginPage";
@@ -14,6 +15,30 @@ import PublicNavbar from "./components/PublicNavBar";
 
 function App() {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const req = { userId: "u_7rgNArdf8we7HYtkdOtHGL84amd2" };
+  const apiKey = "sRwLIUgSxN7xQzIfv1mqODwMEUQO/gIMd0JTtPwYViM=";
+  const token = jwt.sign(req, apiKey);
+  console.log(token);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const url =
+          "https://us-central1-ohyay-prod-d7acf.cloudfunctions.net/ohyayapi/list-workspaces";
+        const res = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "text/plain",
+          },
+          body: token,
+        });
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [token]);
+
   return (
     <>
       <Router>
